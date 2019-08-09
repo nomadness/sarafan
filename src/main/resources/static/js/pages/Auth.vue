cc0217327dea0f5497d172e5da00cca7b073c063
volanaro <vitaliy.skolozdra@gmail.com>
2019-08-09 15:27:40 +0300

    Lesson 12

---
+++ src/main/resources/static/js/pages/Auth.vue
@@ -0,0 +1,12 @@
+<template>
+    <v-container>
+        Необходимо авторизоваться через
+        <a href="/login">Google</a>
+    </v-container>
+</template>
+<script>
+
+</script>
+<style>
+
+</style>
