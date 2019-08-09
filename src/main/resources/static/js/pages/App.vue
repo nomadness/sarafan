8e58834a9af7d7d26f9c7b6efcfaeb52720db965
volanaro <vitaliy.skolozdra@gmail.com>
2019-08-09 14:31:08 +0300

    Lesson 11

--- src/main/resources/static/js/pages/App.vue
+++ src/main/resources/static/js/pages/App.vue
@@ -14,41 +14,35 @@
                 <a href="/login">Google</a>
             </v-container>
             <v-container v-if="profile">
-                <messages-list :messages="messages" />
+                <messages-list/>
             </v-container>
         </v-content>
     </v-app>
 </template>
 
 <script>
+    import { mapState, mapMutations } from 'vuex'
     import MessagesList from 'components/messages/MessageList.vue'
     import { addHandler } from 'util/ws'
     export default {
         components: {
             MessagesList
         },
-        data() {
-            return {
-                messages: frontendData.messages,
-                profile: frontendData.profile
-            }
-        },
+        computed: mapState(['profile']),
+        methods: mapMutations(['addMessageMutation', 'updateMessageMutation', 'removeMessageMutation']),
         created() {
             addHandler(data => {
                 if (data.objectType === 'MESSAGE') {
-                    const index = this.messages.findIndex(item => item.id === data.body.id)
                     switch (data.eventType) {
                         case 'CREATE':
+                            this.addMessageMutation(data.body);
+                            break;
                         case 'UPDATE':
-                            if (index > -1) {
-                                this.messages.splice(index, 1, data.body)
-                            } else {
-                                this.messages.push(data.body)
-                            }
-                            break
+                            this.updateMessageMutation(data.body);
+                            break;
                         case 'REMOVE':
-                            this.messages.splice(index, 1)
-                            break
+                            this.removeMessageMutation(data.body);
+                            break;
                         default:
                             console.error(`Looks like the event type if unknown "${data.eventType}"`)
                     }
